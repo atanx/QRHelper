7dded2e0abf1e926dc49c227ddd630a9043ef7d9
jiangbin <jiangbin@xmov.ai>
2019-07-20 11:27:27 +0800

    增加打开按钮

--- js/options.js
+++ js/options.js
@@ -7,20 +7,22 @@ $(document).ready(function () {
     getCurrentUrl();
     loadHistory();
     showQR();
-    $("#removeBtn").on("click", removeSelection);
+    $("#removeUrl").on("click", removeSelection);
+    $("#openUrl").on("click", openCurrentUrl);
     $("#historyUrls").on("change", showQR);
-    $("#openTabBtn").on("click", function(){window.open('../pages/tool.html')})
-    }
+    $("#openTabBtn").on("click", function () { window.open('../pages/tool.html') });
+    $("#openTabBtn").on("click", function () { window.open('../pages/tool.html') });
+}
 );
 
 /** 获取当前页面链接
  *
  * 通过与tab页通信获取当前页面的链接，然后将其添加到
  * */
-var getCurrentUrl = function(){
+var getCurrentUrl = function () {
     /** 与tab页通信。 */
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {action: 'get_url'}, function (response) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'get_url' }, function (response) {
             if (response && response.url) {
                 var history = new HistoryUrls();
                 history.add(response.url);
@@ -35,55 +37,55 @@ var getCurrentUrl = function(){
 };
 
 /** 历史链接类 */
-var HistoryUrls = function(){
+var HistoryUrls = function () {
     this.urls = [];
     this.keys = [];
     this.length = 0;
     this.ptr = 0;
     this.prefix = "history.url.";
 
-    this.load = function(){
+    this.load = function () {
         this._load_ptr();   // 加载链接数
         this._load_urls();     // 加载链接
     };
 
-    this._load_ptr = function(){
-      var ptr = localStorage[this.prefix+'ptr'] || 0;
-      this.ptr = parseInt(ptr);
+    this._load_ptr = function () {
+        var ptr = localStorage[this.prefix + 'ptr'] || 0;
+        this.ptr = parseInt(ptr);
     };
 
-    this._save_ptr = function(){
-        localStorage[this.prefix+'ptr'] = this.ptr;
+    this._save_ptr = function () {
+        localStorage[this.prefix + 'ptr'] = this.ptr;
     };
 
-    this._load_urls = function(){
+    this._load_urls = function () {
         this.urls = [];
         this.keys = [];
-      for(var i = 1; i<=this.ptr; i++){
-          var key = this.prefix+i;
-          var url = localStorage[key];
-          if(url){
-              this.urls.push(url);
-              this.keys.push(key);
-          }
-      }
-      this.length = this.urls.length;
+        for (var i = 1; i <= this.ptr; i++) {
+            var key = this.prefix + i;
+            var url = localStorage[key];
+            if (url) {
+                this.urls.push(url);
+                this.keys.push(key);
+            }
+        }
+        this.length = this.urls.length;
     };
-    this.add = function(url) {
-        this.ptr = this.ptr+1;
+    this.add = function (url) {
+        this.ptr = this.ptr + 1;
         this._save_ptr();
 
-        var key = this.prefix+this.ptr;
+        var key = this.prefix + this.ptr;
         this.keys.push(key);
         this.urls.push(url);
         localStorage[key] = url;
     };
 
-    this.removeByKey = function(key){
+    this.removeByKey = function (key) {
         /* 从keys和urls中移除*/
         var index = this.keys.indexOf(key);
 
-        if(index>=0){
+        if (index >= 0) {
             this.keys.splice(index, 1);
             this.urls.splice(index, 1);
             this._save_ptr();
@@ -92,26 +94,26 @@ var HistoryUrls = function(){
 
     };
 
-    this.removeAll = function(){
-        for(var key in localStorage){
-            if(key.indexOf(this.prefix)>=0){
+    this.removeAll = function () {
+        for (var key in localStorage) {
+            if (key.indexOf(this.prefix) >= 0) {
                 localStorage.removeItem(key);
-                this.ptr=0;
+                this.ptr = 0;
                 this.urls = [];
                 this.keys = [];
                 this.length = 0;
             }
         }
     };
-    this.toOptions = function(){
+    this.toOptions = function () {
         var optionStr = '';
         var template = '<option name="{key}" value="{url}">{url}</option>';
-        for(var i=this.length-1; i>=0; i--){
+        for (var i = this.length - 1; i >= 0; i--) {
             var key = this.keys[i];
             var url = this.urls[i];
             var line = template.replace(/{key}/g, key);
             line = line.replace(/{url}/g, url);
-            optionStr += '\n'+line;
+            optionStr += '\n' + line;
         }
         return optionStr;
     };
@@ -119,21 +121,27 @@ var HistoryUrls = function(){
 };
 
 /** 加载历史链接 */
-var loadHistory = function(){
-   var history = new HistoryUrls();
-   $('#historyUrls').html(history.toOptions());
+var loadHistory = function () {
+    var history = new HistoryUrls();
+    $('#historyUrls').html(history.toOptions());
 };
 
 /** 删除当前选择 */
-var removeSelection = function(){
-   var key = $("#historyUrls option:selected").attr("name");
-   var history = new HistoryUrls();
-   history.removeByKey(key);
-   $("#historyUrls").html(history.toOptions());
-   //showQR();
+var removeSelection = function () {
+    var key = $("#historyUrls option:selected").attr("name");
+    var history = new HistoryUrls();
+    history.removeByKey(key);
+    $("#historyUrls").html(history.toOptions());
+    //showQR();
 };
 
-var showQR = function(){
+/** 打开当前链接 */
+var openCurrentUrl = function () {
+    var url = $('#historyUrls').val();
+    window.open(url);
+};
+
+var showQR = function () {
     var url = $('#historyUrls').val();
     $("#tips").text("(使用微信扫一扫查看链接)");
     $("#url").text(url);
